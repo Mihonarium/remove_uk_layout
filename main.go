d7ddba88bc490ce3c08610f13131e3536aa748fc
Mikhail Samin <i@mishasamin.xyz>
2024-01-12 12:48:33 +0000

    Update main.go

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	if err := deleteKeyboardLayout("00000809"); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	if err := deleteKeyboardLayout("0x00000809"); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	fmt.Println("Finished deleting keyboard layout entries. Please sign out & sign back in or restart your computer.")
 
@@ -30,6 +30,41 @@ func deleteKeyboardLayout(keyboardID string) error {
 	// Delete from Preload using both the keyboardID and substitute IDs
 	if err := deleteFromPreload(registry.CURRENT_USER, `Keyboard Layout\Preload`, keyboardID, substituteIDs); err != nil {
 		return fmt.Errorf("error deleting from Preload: %w", err)
+	}
+
+	// do the same for all HKEY_USERS (keys like "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload")
+
+	// read all subkeys of HKEY_USERS
+	key, err := registry.OpenKey(registry.USERS, "", registry.QUERY_VALUE|registry.ENUMERATE_SUB_KEYS)
+	if err != nil {
+		return fmt.Errorf("unable to open registry key: %w", err)
+	}
+	defer func(key registry.Key) {
+		err := key.Close()
+		if err != nil {
+			fmt.Println("error closing registry key:", err)
+		}
+	}(key)
+
+	fmt.Println(key)
+
+	subKeys, err := key.ReadSubKeyNames(0)
+	if err != nil {
+		return fmt.Errorf("error reading subkeys: %w", err)
+	}
+
+	for _, subKey := range subKeys {
+		fmt.Println("Processing subkey:", subKey)
+		// delete from Substitutes
+		substituteIDs, err := deleteFromRegistryAndGetSubstitutes(registry.USERS, fmt.Sprintf(`%s\Keyboard Layout\Substitutes`, subKey), keyboardID)
+		if err != nil {
+			return fmt.Errorf("error deleting from Substitutes: %w", err)
+		}
+
+		// delete from Preload
+		if err := deleteFromPreload(registry.USERS, fmt.Sprintf(`%s\Keyboard Layout\Preload`, subKey), keyboardID, substituteIDs); err != nil {
+			return fmt.Errorf("error deleting from Preload: %w", err)
+		}
 	}
 
 	return nil
